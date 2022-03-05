129d17420afd280b9a9e0872864e24a8164c2ac7
Abid Al Wassie <abidalwassie@outlook.com>
2022-03-05 23:51:35 +0600

    updated project

--- timeConverter/js/main.js
+++ timeConverter/js/main.js
@@ -1,28 +1,36 @@
+function m2s() {
 
-var time = "10:30:00"; // your input
+  const time = document.getElementById("timeValue").value.split(':'); // convert to array
 
-time = time.split(':'); // convert to array
-
-// fetch
-var hours = Number(time[0]);
-var minutes = Number(time[1]);
-var seconds = Number(time[2]);
-
-// calculate
-var timeValue;
-
-if (hours > 0 && hours <= 12) {
-  timeValue= "" + hours;
-} else if (hours > 12) {
-  timeValue= "" + (hours - 12);
-} else if (hours == 0) {
-  timeValue= "12";
+  // fetch
+  var hours = Number(time[0]);
+  var minutes = Number(time[1]);
+  var seconds = Number(time[2]);
+  
+  // calculate
+  var timeValue;
+  
+  if (hours > 0 && hours <= 12) {
+    timeValue= "" + hours;
+  } else if (hours > 12) {
+    timeValue= "" + (hours - 12);
+  } else if (hours == 0) {
+    timeValue= "12";
+  }
+  
+  timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+  timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
+  timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+  
+  // show
+  // alert(timeValue);
+  
+  const militaryPlaceholder = document.getElementById("cmvalue");
+  
+  // const militaryValue
+  militaryPlaceholder.innerText = timeValue;
+  console.log(timeValue);
 }
 
-timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
-timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
-timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+// var time = "21:27:00"; // your input
 
-// show
-// alert(timeValue);
-console.log(timeValue);

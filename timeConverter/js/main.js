875aa298d04ac89fa8c9808ba443b12e6905eef3
Abid Al Wassie <abidalwassie@outlook.com>
2022-03-18 00:54:05 +0600

    updated project

--- timeConverter/js/main.js
+++ timeConverter/js/main.js
@@ -1,10 +1,10 @@
 function m2s() {
 
-  const h = document.getElementById("h").value; // get hour
-  const m = document.getElementById("m").value; // get minute
+  const h1 = document.getElementById("h").value; // get hour
+  const m1 = document.getElementById("m").value; // get minute
 
-  var hours = Number(h);
-  var minutes = Number(m);
+  var hours = Number(h1);
+  var minutes = Number(m1);
   
   // calculate
   var timeValue;
@@ -18,8 +18,11 @@ function m2s() {
     } else if (hours == 0) {
       timeValue= "12";
     }
+
   } else {
     alert("invalid hour");
+    var militaryPlaceholder = document.getElementById("cmvalue");
+    militaryPlaceholder.innerText = "timeValue";
   }
   
   timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
@@ -28,12 +31,54 @@ function m2s() {
   // show
   // alert(timeValue);
   
-  const militaryPlaceholder = document.getElementById("cmvalue");
+  var militaryPlaceholder = document.getElementById("cmvalue");
   
   // const militaryValue
   militaryPlaceholder.innerText = timeValue;
-  console.log(timeValue);
+  // console.log(timeValue);
 }
 
 // var time = "21:27:00"; // your input
 
+
+
+
+// var strUser = e.value;
+
+function s2m() {
+
+  var e = document.getElementById("ampmValue");
+  const h2 = document.getElementById("h2").value;
+  const m2 = document.getElementById("m2").value;
+  
+  // const time12h = "01:02 PM";
+  const time12h = h2 + ':' + m2 + ' ' + e.value;
+  console.log(time12h);
+  // console.log(e.value);
+
+
+const convertTime12to24 = (time12h) => {
+  const [time, modifier] = time12h.split(' ');
+
+  let [hours, minutes] = time.split(':');
+
+  if (hours === '12') {
+    hours = '00';
+  }
+
+  if (modifier === 'PM') {
+    hours = parseInt(hours, 10) + 12;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+const standardPlaceholder = document.getElementById("csvalue");
+  
+// show standard time
+standardPlaceholder.innerText = convertTime12to24(time12h);
+
+// console.log(convertTime12to24(time12h));
+
+}
+// s2m();
